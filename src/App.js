03aa008b0274fbb79b44e66fcab8d7b5802e370b
Reslea <rmidion@gmail.com>
2021-07-07 22:32:28 +0300

    use heroku

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch("https://rembo-test.herokuapp.com/api/hello/message")
+      .then(r => setData(r.json()))
+  }, []);
+
   return (
     <div className="App">
-      <h1>update test</h1>
+      <h1>{data.message}</h1>
     </div>
   );
 }
