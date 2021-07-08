1938275ed4aeea336bdbfa87acbd63cdca74243f
Sergey Zhizhko <sergey.zhizhko@dataart.com>
2021-07-08 19:59:43 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 function App() {
-  const [data, setData] = useState();
 
-  useEffect(() => {
-    fetch("https://rembo-test.herokuapp.com/api/hello/message")
-      .then(async (r) => setData(await r.json()))
-  }, []);
-
-  console.log(data);
   return (
     <div className="App">
-      <h1>{data?.message}</h1>
+      <h1>Test run</h1>
     </div>
   );
 }
